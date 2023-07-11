fee4cfd18e79949dfde94e8044ad47827f3b2971
Vadim Peregudov <vadim.peregudov.007@gmail.com>
2023-07-11 22:03:57 +0700

    Task completed

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface UserDao {
    void add(User user);
    List<User> listUsers();
+
+   List<User> findUsersByCarModelAndSeries(String model, int series);
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,7 +1,9 @@
 package hiber.dao;
 
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -11,19 +13,30 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public List<User> findUsersByCarModelAndSeries(String model, int series) {
+        Session session = sessionFactory.getCurrentSession();
+
+        Query query = session.createQuery(" FROM User u where u.car.model = :modelCar and u.car.series = :seriesCar");
+        query.setParameter("modelCar", model);
+        query.setParameter("seriesCar", series);
+
+        return query.getResultList();
+    }
 
 }
